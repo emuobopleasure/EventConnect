d5536355b3fe39bc421cca15c6bdb023d38a9b49
Davis06 <davis06@outlook.com>
2023-08-27 19:19:13 +0100

    Utilized the useParams hook to access the currently active route parameter (eventId) identifier within the route definitions to dynamically fetch and utilize specific data related to the event identified by the eventId parameter

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const EventDetailPage = () => {
+  const params = useParams()
   return (
-    <div>EventDetailPage</div>
+    <>
+      <h1>EventDetailsPage</h1>
+      <p>Ecent ID: {params.eventId}</p>
+    </>
   )
 }
 
