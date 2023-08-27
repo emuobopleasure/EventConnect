390c702f154806893829c91d0362a56c66b6934a
Davis06 <davis06@outlook.com>
2023-08-27 19:07:53 +0100

    Rendered a list of dummy events on the events page and also incorporated links to individual event pages

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'someEvent'
+  },
+  {
+    id: 'e2',
+    title: 'anotherEvent'
+  }
+]
 
 const EventsPage = () => {
   return (
-    <div>EventsPage</div>
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {
+          DUMMY_EVENTS.map((event) => (
+            <li key={event.id}>
+              <Link to={`/events/${event.id}`}>
+                {event.title}
+              </Link>
+            </li>
+
+          ))
+        }
+      </ul>
+    </>
   )
 }
 
