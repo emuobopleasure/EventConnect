734479af189bcfa728129c5488ae189aff9312e7
Davis06 <davis06@outlook.com>
2023-09-04 20:17:18 +0100

    Utilized the react-router-json function to streamline the process of building responses within the application, eliminating the need for manual construction.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,5 @@
 
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
@@ -19,12 +19,14 @@ function EventsPage() {
 export default EventsPage;
 
 export const loader = async () => {
-  const response = await fetch('http://localhost:8080/eventsfddg')
+  const response = await fetch('http://localhost:8080/events')
   if (!response.ok) {
-    throw Response(JSON.stringify({message: 'Could not fetch events.'}),
-      { status: 500 }
-    )
+    // throw Response(JSON.stringify({message: 'Could not fetch events.'}),
+    //   { status: 500 }
+    // )
     // return { isError: true, message: 'Could not fetch events'}
+    throw json({message: 'Could not fetch events.'},
+      { status: 500 })
   } else {
     return response
   }
