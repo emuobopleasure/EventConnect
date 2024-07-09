24b8236352dcae3d3500dafc83e886d6faadeb97
Davis06 <davis06@outlook.com>
2024-07-09 23:16:42 +0100

    Implemented the defer feature from React Router to display partial content while waiting for other content to load within the same component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,33 +1,41 @@
 
-import { json, useLoaderData } from 'react-router-dom';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-  const data = useLoaderData()
-  // if (data.isError) {
-  //   return data.message
-  // }
-  const events = data.events
+  const { events } = useLoaderData()
+
+
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: 'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadEvents) => <EventsList events={loadEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events')
   if (!response.ok) {
     // throw Response(JSON.stringify({message: 'Could not fetch events.'}),
     //   { status: 500 }
     // )
     // return { isError: true, message: 'Could not fetch events'}
-    throw json({message: 'Could not fetch events.'},
+    throw json({ message: 'Could not fetch events.' },
       { status: 500 })
   } else {
-    return response
+    const resData = await response.json()
+    return resData.events
   }
+}
+
+export const loader = () => {
+  return defer({
+    events: loadEvents()
+  })
 }
